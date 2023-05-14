5a0b1d5f04d6a291e34b1f7119427adabb38935f
Fabiola Magalhaes <fabimagal99@gmail.com>
2023-05-13 21:41:29 -0300

    Exercício 6. O Programa vai receber o nome, preço e quantidade de certo produto.

--- src/Entidades/Produtos.java
+++ src/Entidades/Produtos.java
@@ -10,4 +10,13 @@ public class Produtos {
         this.preco = preco;
         this.quantidade = quantidade;
     }
+
+    public String getNome() { return nome; }
+    public Double getPreco() { return preco; }
+    public int getQuantidade() { return quantidade; }
+
+    public void setNome(String nome) { this.nome = nome; }
+    public void setPreco(Double preco) { this.preco = preco; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import Entidades.Produtos;
 public class Main {
     public static void main(String[] args) {
 
-        Produtos produto = new Produtos("Café",35.0, 80);
+        Produtos produto = new Produtos("café",35.0, 80);
+
+        System.out.println("Há " + produto.getQuantidade() + " pacotes de "
+                + produto.getNome() + " no estoque no valor de  " + produto.getPreco());
+
     }
 }
